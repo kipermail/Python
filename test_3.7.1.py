19c682a03c9d01a553922eeee1c0f3e906174949
kipermail <kiper_mail@i.ua>
2023-07-16 18:00:07 +0300

    Small Code Refactoring

--- test_3.7.1.py
+++ test_3.7.1.py
@@ -2,15 +2,12 @@ numstr = int(input())
 comanddict = dict()
 for i in range(numstr):
     str = input().split(";")
-    comd1 = str[0]
-    comd2 = str[2]
-    goal1 = int(str[1])
-    goal2 = int(str[3])
+    comd1, goal1, comd2, goal2 = str
 
     if comanddict.get(comd1) is None:
-        comanddict[comd1] = [0, 0, 0, 0, 0]
+        comanddict[comd1] = [0] * 5
     if comanddict.get(comd2) is None:
-        comanddict[comd2] = [0, 0, 0, 0, 0]
+        comanddict[comd2] = [0] * 5
 
     res1 = comanddict[comd1]
     res1[0] += 1
@@ -19,16 +16,13 @@ for i in range(numstr):
 
     if goal1 == goal2:
         res1[2] += 1
-        res1[4] += 1  # Point
         res2[2] += 1
-        res2[4] += 1  # Point
     elif goal1 > goal2:
         res1[1] += 1
-        res1[4] += 3  # Point
         res2[3] += 1
     elif goal2 > goal1:
         res2[1] += 1
-        res2[4] += 3  # Point
         res1[3] += 1
 for i in comanddict:
-     print(f'{i}:', *comanddict[i], end='\n')
+    comanddict[i][4] = comanddict[i][1] * 3 + comanddict[i][2] * 1        
+    print(f'{i}:', *comanddict[i], end='\n')
